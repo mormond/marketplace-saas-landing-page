6e4b44da2ed2a3ed01bc763f6f12645edb6c89c9
Mike Ormond <mormond@users.noreply.github.com>
2022-02-16 15:50:26 +0000

    Fixing debugging

---
+++ api/message/__init__.py
@@ -0,0 +1,18 @@
+import json
+import logging
+
+import azure.functions as func
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    # name = req.params.get('name')
+    # if not name:
+    #     try:
+    #         req_body = req.get_json()
+    #     except ValueError:
+    #         pass
+    #     else:
+    #         name = req_body.get('name')
+
+    return func.HttpResponse(json.dumps( { "text": "Hello from the API" }) )
