402bc2ea16b94d1edd8cd6da04d2267cc0e124d3
Mike Ormond <mormond@users.noreply.github.com>
2022-02-17 16:07:45 +0000

    Basic token decode working

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React, { useState, useEffect } from 'react';
+import { useSearchParams } from "react-router-dom";
 
 function App() {
   const [data, setData] = useState('');
 
+  const [searchParams] = useSearchParams();
+  const queryToken = searchParams.get('token');
+
+  const Log = ({ value, replacer = null, space = 2 }) => (
+    <pre>
+      <code>{JSON.stringify(value, replacer, space)}</code>
+    </pre>
+  )
+
   useEffect(() => {
     (async function () {
-      const { text } = await( await fetch(`/api/message`)).json();
+      const { text } = await (await fetch(`/api/message`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ marketplace_token: queryToken })
+      })).json();
       setData(text);
-    })();
-  });
 
-  return <div>{data}</div>;
+    })();
+  }, []);
+
+  return <div><Log value={data}></Log></div>;
 }
 
 export default App;
+
