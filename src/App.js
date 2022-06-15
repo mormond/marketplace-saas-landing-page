efaf6559e02eda2220f6dfdd9ca3d840b86759d9
Mike Ormond <mormond@users.noreply.github.com>
2022-06-15 14:11:50 +0000

    Refresh UI

--- src/App.js
+++ src/App.js
@@ -1,42 +1,68 @@
 import React, { useState, useEffect } from 'react';
-import { useSearchParams } from "react-router-dom";
-
-function AuthButton() {
-  const authorise = () => {
-    // Call the authorise API
-    console.log("Hello");
-  }
-  return <button onClick={authorise}>Authorise</button>;
-}
+import { useSearchParams } from 'react-router-dom';
+import './index.css';
 
 function App() {
-  const [data, setData] = useState('');
-
+  const [decodedToken, setDecodedToken] = useState({});
+  const [response, setResponse] = useState('');
   const [searchParams] = useSearchParams();
   const queryToken = searchParams.get('token');
 
-  const Log = ({ value, replacer = null, space = 2 }) => (
+  const Log = ({ isJson=false, value, replacer = null, space = 2 }) => (
     <pre>
-      <code>{JSON.stringify(value, replacer, space)}</code>
+      {isJson === true ? <code>{JSON.stringify(value, replacer, space)}</code> : <code>{value}</code>}
     </pre>
   )
-  
+
   useEffect(() => {
-    (async function () {
-      const { text } = await (await fetch(`/api/resolve_token`, {
+    if (response) {
+      console.log(response);
+      const tokenObject = JSON.parse(response);
+      setDecodedToken(tokenObject);
+    }
+  }, [response]);
+
+  async function decodeToken() {
+    try {
+      const token = await (await fetch(`/api/resolve_token`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
         },
         body: JSON.stringify({ marketplace_token: queryToken })
       })).json();
-      setData(text);
 
-    })();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+      setResponse(JSON.stringify(token.text, null, 2));
+      setDecodedToken(JSON.parse(response));
 
-  return <div><div><Log value={data}></Log></div><div><AuthButton /></div></div>;
+    } catch (error) {
+      return;
+    }
+  }
+
+  return <div className='maincontainer'>
+    <header>
+      <h1>Azure Marketplace</h1>
+      <h2>SaaS Offer Landing Page Sample</h2>
+    </header>
+
+    <div>
+      <h3>Raw Token</h3>
+      <Log value={queryToken ? queryToken : 'No token found to decode. Is the token parameter present in the query string?'} />
+    </div>
+
+    <div>
+      <h3>Decoded Token</h3>
+      <Log isJson={true} value={decodedToken} />
+    </div>
+
+    <button onClick={() => decodeToken()}
+      className="button">
+      Decode Token
+    </button>
+
+    <footer style={{ marginTop: 10 + '%' }}>For more information see <a href='https://github.com/mormond/marketplace-saas-landing-page'>https://github.com/mormond/marketplace-saas-landing-page</a></footer>
+  </div>
 }
 
 export default App;
