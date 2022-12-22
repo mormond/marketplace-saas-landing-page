785483249ab0cd3eb03ec598fab6890733d40b83
Mike Ormond <13424886+mormond@users.noreply.github.com>
2022-12-22 13:33:15 +0000

    Fixed

--- api/resolve_token/__init__.py
+++ api/resolve_token/__init__.py
@@ -38,7 +38,46 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     dump_full_token = True
 
     if dump_full_token is True:
-        my_token = resolved_token
+        my_token = {
+            "id": resolved_token.id,
+            "subscriptionName": resolved_token.subscription_name,
+            "offerId": resolved_token.offer_id,
+            "planId": resolved_token.plan_id,
+            "quantity": resolved_token.quantity,
+            "subscription": {
+                "id": resolved_token.subscription.id,
+                "publisherId": resolved_token.subscription.publisher_id,
+                "offerId": resolved_token.subscription.offer_id,
+                "name": resolved_token.subscription.name,
+                "saasSubscriptionStatus": resolved_token.subscription.saas_subscription_status,
+                "beneficiary": {
+                    "emailId": resolved_token.subscription.beneficiary.email_id,
+                    "objectId": resolved_token.subscription.beneficiary.object_id,
+                    "tenantId": resolved_token.subscription.beneficiary.tenant_id,
+                    "puid": resolved_token.subscription.beneficiary.puid
+                },
+                "purchaser": {
+                    "emailId": resolved_token.subscription.purchaser.email_id,
+                    "objectId": resolved_token.subscription.purchaser.object_id,
+                    "tenantId": resolved_token.subscription.purchaser.tenant_id,
+                    "puid": resolved_token.subscription.purchaser.puid
+                },
+                "planId": resolved_token.subscription.plan_id,
+                "term": {
+                    # "termUnit": resolved_token.subscription.term.term_unit,
+                    "startDate": resolved_token.subscription.term.start_date,
+                    "endDate": resolved_token.subscription.term.end_date
+                },
+                # "autoRenew": resolved_token.subscription.auto_renew,
+                "isTest": resolved_token.subscription.is_test,
+                "isFreeTrial": resolved_token.subscription.is_free_trial,
+                "allowedCustomerOperations": resolved_token.subscription.allowed_customer_operations,
+                "sandboxType": resolved_token.subscription.sandbox_type,
+                # "lastModified": resolved_token.subscription.last_modified,
+                "quantity": resolved_token.subscription.quantity,
+                "sessionMode": resolved_token.subscription.session_mode
+            }
+        }
     else:
         my_token = {
             "id": resolved_token.id,
@@ -46,24 +85,30 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             "offerId": resolved_token.offer_id,
             "planId": resolved_token.plan_id,
             "quantity": resolved_token.quantity,
-            "SubscriptionPublisherId": resolved_token.subscription.publisher_id,
-            "saasSubscriptionStatus": resolved_token.subscription.saas_subscription_status,
-            "beneficiaryEmailId": resolved_token.subscription.beneficiary.email_id,
-            "beneficiaryObjectId": resolved_token.subscription.beneficiary.object_id,
-            "beneficiaryTenantId": resolved_token.subscription.beneficiary.tenant_id,
-            "beneficiaryPid": resolved_token.subscription.beneficiary.puid,
-            "purchaserEmailId": resolved_token.subscription.purchaser.email_id,
-            "purchaserObjectId": resolved_token.subscription.purchaser.object_id,
-            "purchaserTenantId": resolved_token.subscription.purchaser.tenant_id,
-            "purchaserPid": resolved_token.subscription.purchaser.puid,
-            # "termTermUnit": resolved_token.subscription.term.term_unit,
-            "termStartDate": resolved_token.subscription.term.start_date,
-            "termEndDate": resolved_token.subscription.term.end_date,
-            "isTest": resolved_token.subscription.is_test,
-            "isFreeTrial": resolved_token.subscription.is_free_trial,
-            "allowedCustomerOperations": resolved_token.subscription.allowed_customer_operations,
-            "sandboxType": resolved_token.subscription.sandbox_type,
-            "sessionMode": resolved_token.subscription.session_mode
+            "subscription": {
+                "id": resolved_token.subscription.id,
+                "publisherId": resolved_token.subscription.publisher_id,
+                "name": resolved_token.subscription.name,
+                "saasSubscriptionStatus": resolved_token.subscription.saas_subscription_status,
+                "beneficiary": {
+                    "emailId": resolved_token.subscription.beneficiary.email_id,
+                },
+                "purchaser": {
+                    "emailId": resolved_token.subscription.purchaser.email_id,
+                },
+                "term": {
+                    # "termUnit": resolved_token.subscription.term.term_unit,
+                    "startDate": resolved_token.subscription.term.start_date,
+                    "endDate": resolved_token.subscription.term.end_date
+                },
+                # "autoRenew": resolved_token.subscription.auto_renew,
+                "isTest": resolved_token.subscription.is_test,
+                "isFreeTrial": resolved_token.subscription.is_free_trial,
+                "allowedCustomerOperations": resolved_token.subscription.allowed_customer_operations,
+                "sandboxType": resolved_token.subscription.sandbox_type,
+                # "lastModified": resolved_token.subscription.last_modified,
+                "sessionMode": resolved_token.subscription.session_mode
+            }
         }
 
-    return func.HttpResponse(json.dumps({ "text": my_token }))
+    return func.HttpResponse(json.dumps({"text": my_token}))
